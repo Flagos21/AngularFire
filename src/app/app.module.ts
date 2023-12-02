c441bd7c211c57f30832e49c899845563686beda
Fabian <flagosp21@gmail.com>
2023-12-02 16:51:13 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"angularfire-d6998","appId":"1:815137832818:web:2568f85f147d778c0567eb","storageBucket":"angularfire-d6998.appspot.com","apiKey":"AIzaSyCSWd1UTSa2RIs1ozPNHexanSoRXICucr0","authDomain":"angularfire-d6998.firebaseapp.com","messagingSenderId":"815137832818","measurementId":"G-ZYB82MFVT2"})),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
